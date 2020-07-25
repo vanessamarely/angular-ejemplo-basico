6ee0b1f517f441dd945706e4dfba22e66f4a66e9
vanessamarely <vanessamarely@gmail.com>
2020-07-25 12:52:26 -0500

    2 files modified

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -7,7 +7,7 @@ export class LoginService {
   private apiRoot: string = 'https://api.github.com/users/';
 
   constructor(private http: HttpClient) { }
-
+  // si incluimos despues de ${username} el /repos, me traeria la lista de repositorios
   getApi(username: string) {
     return this.http.get(`${this.apiRoot}${username}`);
   }
